6fc1969a99148d4e175ae927a51f3b07757d456a
Tomas Zwolinski <toza21@student.bth.se>
2022-08-31 14:09:23 +0200

    add skeleton for editor poject

---
+++ src/components/TipTap.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+
+<template>
+    <h1>TipTap Editor</h1>
+</template>
+
+<style>
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup>
+import TipTap from "@/components/TipTap.vue";
+</script>
+
+<template>
+  <main>
+    <TipTap />
+  </main>
+</template>
