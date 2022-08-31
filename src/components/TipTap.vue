9ddf0e7ebe7e2a8f9b40176220cf82d0923d96a3
Tomas Zwolinski <toza21@student.bth.se>
2022-08-31 18:01:54 +0200

    add a few options for the editor

--- src/components/TipTap.vue
+++ src/components/TipTap.vue
@@ -14,6 +14,36 @@ const printContent = (text) => {
 
 <template>
   <div v-if="editor" class="menu">
+    <button
+      @click="editor.chain().focus().toggleBold().run()"
+      :class="{ 'is-active': editor.isActive('bold') }"
+    >
+      bold
+    </button>
+    <button
+      @click="editor.chain().focus().toggleItalic().run()"
+      :class="{ 'is-active': editor.isActive('italic') }"
+    >
+      italic
+    </button>
+    <button
+      @click="editor.chain().focus().toggleHeading({ level: 1 }).run()"
+      :class="{ 'is-active': editor.isActive('heading', { level: 1 }) }"
+    >
+      h1
+    </button>
+    <button
+      @click="editor.chain().focus().toggleHeading({ level: 2 }).run()"
+      :class="{ 'is-active': editor.isActive('heading', { level: 2 }) }"
+    >
+      h2
+    </button>
+    <button
+      @click="editor.chain().focus().toggleHeading({ level: 3 }).run()"
+      :class="{ 'is-active': editor.isActive('heading', { level: 3 }) }"
+    >
+      h3
+    </button>
     <button @click="printContent(editor.getHTML())">console log</button>
   </div>
   <editor-content :editor="editor" />
