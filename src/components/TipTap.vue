5c4c274fc89c16dfbb204be632e2ef65019dc15c
Tomas Zwolinski <toza21@student.bth.se>
2022-08-31 14:58:53 +0200

    add a basic TipTap editor

--- src/components/TipTap.vue
+++ src/components/TipTap.vue
@@ -1,11 +1,37 @@
-<script setup>
+<script>
+import { useEditor, EditorContent } from '@tiptap/vue-3'
+import StarterKit from '@tiptap/starter-kit'
 
+export default {
+  components: {
+    EditorContent,
+  },
+
+  setup() {
+    const editor = useEditor({
+      content: '<p>I’m running Tiptap with Vue.js. 🎉</p>',
+      extensions: [
+        StarterKit,
+      ],
+    })
+
+    return { editor }
+  },
+}
 </script>
 
 <template>
-    <h1>TipTap Editor</h1>
+    <editor-content :editor="editor" />
 </template>
 
 <style>
+/* Basic editor styles */
+.ProseMirror {
+  min-height: 40vh;
+  margin: 2em;
+    border: 1px solid black;
+    border-radius: 5px;
+    padding: 5px;
 
+}
 </style>
