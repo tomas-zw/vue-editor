255dd6936cc606cce47f209aad5c5b9e1a4af7b0
Tomas Zwolinski <toza21@student.bth.se>
2022-09-15 13:25:32 +0200

    add tests with cypress

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,36 @@
+describe('My first tests', () => {
+    before(() => {
+      cy.visit('http://localhost:5173/')
+  })
+
+  it('Menu, titlebar and editor exists', () => {
+        cy.get('.menu').contains('Spara')
+        cy.get('.menu').contains('nytt dokument')
+        cy.get('.menu').contains('Välj dokument')
+        cy.get('.title-bar')
+          .should('have.value','')
+        cy.get('.ProseMirror')
+          .should('contain','Skapa ett nytt dokument eller välj ett befintligt från menyn.')
+  })
+
+  it('Edit title and body in a new document', () => {
+        cy.contains('nytt dokument').click()
+        cy.get('.title-bar')
+          .type('This is a new Title')
+          .should('have.value','This is a new Title')
+        cy.get('.ProseMirror')
+          .should('contain','')
+          .type('This is a new Body')
+          .find('p')
+          .should('contain','This is a new Body')
+  })
+
+  it('Open "MongoDB Atlas" dokument', () => {
+        cy.contains('Välj dokument').focus()
+        cy.get('.dropdown-content')
+          .contains('MongoDB Atlas').click({ force: true})
+        cy.get('.title-bar')
+          .should('have.value','MongoDB Atlas')
+  })
+
+})
